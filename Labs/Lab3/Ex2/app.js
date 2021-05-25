28bea84a4fc3e97c91abf2819107d737c8e17732
campfist <77853363+campfist@users.noreply.github.com>
2021-05-24 22:05:55 -0400

    lab 3 rework

--- Labs/Lab3/Ex2/app.js
+++ Labs/Lab3/Ex2/app.js
@@ -1,9 +1,9 @@
 //Declare global variables
-y = 300;
+var y = 300;
 
 //Create canvas
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(2100, 2100);
 }
 
 //Setup draw function & determine background color & 
@@ -11,30 +11,30 @@ function draw() {
     background(255);
 
     //draw 25 black circles
-    for(var x = 0; x < 25; x++) {
+    for(let x = 1; x < 26; x++) {
 
         //draw a blue square if the iterand is divisible by 3 AND 5
         if(x % 3 == 0 && x % 5 == 0) {
             fill('blue');
-            square(x, y, 25);
+            square(x * 50, y, 25);
         } 
         
         //draw a green square if the iterand is divisible by 5
         else if(x % 5 === 0) {
             fill('green');
-            square(x, y, 25)
+            square(x * 50, y, 25)
         }
 
         //draw a purple circle if the iterand is divisible by 3
         else if(x % 3 === 0) {
             fill('purple');
-            circle(x, y, 25);
+            circle(x * 50, y, 25);
         }
 
         //Otherwise draw black circle
         else {
             fill('black');
-            circle(x, y, 25);
+            circle(x * 50, y, 25);
         }
     }
 }
--- Labs/Lab3/Ex3/app.js
+++ Labs/Lab3/Ex3/app.js
@@ -12,12 +12,11 @@ function draw() {
     background(255);
 
     //Create for loop & determine conditions
-    for(var a = 0; a < 4; a++) {
+    for(let a = 0; a < 4; a++) {
 
         //Create another for loop to determine how many red squares will be placed in each row
-        for(var b = 0; b <= a; b++){
-            fill('red');
-            square(x, y, 25);
+        for(let b = 0; b <= a; b++){
+            square(b * 50, a * 50, 50);
         }
     }
 }
