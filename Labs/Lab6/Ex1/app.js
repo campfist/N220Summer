e7d042773b86c6f21b7ac2b957e031f9d83962be
campfist <77853363+campfist@users.noreply.github.com>
2021-06-03 17:50:59 -0400

    Lab 6 rework part 2

--- Labs/Lab6/Ex1/app.js
+++ Labs/Lab6/Ex1/app.js
@@ -1,3 +1,8 @@
+//Create variables that call Divs
+let dogDiv = document.getElementById("dogDiv");
+let dogEnergy = document.getElementById("dogEnergy");
+let dogHappiness = document.getElementById("dogHappiness");
+
 //Create dog object and display the dog's name
 let myDog = {
     name: "Zuko",
@@ -7,14 +12,19 @@ let myDog = {
 };
 console.log(myDog.name);
 
-//Create feedDog function and display the dog's energy
+//Display dog's name
+dogDiv.innerHTML = "Name: " + myDog.name;
+
+//Create feedDog function and display the dog's energy in the dogEnergy div
 function feedDog() {
     myDog.energy = myDog.energy + 5;
+    dogEnergy.innerHTML = "Energy: " + myDog.energy;
     console.log(myDog.energy);
 }
 
-//Create playWithDog function and display the dog's happiness
+//Create playWithDog function and display the dog's happiness in the dogHappiness div
 function playWithDog() {
     myDog.happiness = myDog.happiness + 5; 
+    dogHappiness.innerHTML = "Happiness: " + myDog.happiness;
     console.log(myDog.happiness);
-} 
+}
