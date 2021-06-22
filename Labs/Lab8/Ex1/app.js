0684c90fb7689991e0c74c1fa1910e32383d2ede
campfist <77853363+campfist@users.noreply.github.com>
2021-06-22 18:29:51 -0400

    lab rework 21

--- Labs/Lab8/Ex1/app.js
+++ Labs/Lab8/Ex1/app.js
@@ -5,12 +5,15 @@ let result = document.getElementById("result");
 //Create function for the button
 function divide() {
 
+    //Create variable for value called from the input field
     let value = Number(number.value);
     //console.log(number.value);
 
+    //Create variable for taking the value entered in the input field divided by 7
     let divisible = value % 7;
     //console.log(divisible);
 
+    //Return 0 if the value is divisible by 7
     return value % 7 == 0;
 }
 
@@ -19,6 +22,7 @@ function check() {
 
     //console.log(divide());
 
+    //Create an if/else statement to display true if the value is divisible by 7 or false if the value isn't divisible by 7
     if(divide()) {
         result.innerHTML = "True";
     } else {
